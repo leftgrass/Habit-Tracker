c0dee4c85f257a960377d4933218584e864f2bcf
Your Name <you@example.com>
2026-01-05 03:48:22 +0530

    Reset tour to first step on page visit

--- components/tour/Tour.tsx
+++ components/tour/Tour.tsx
@@ -56,6 +56,7 @@ export function Tour() {
   useEffect(() => {
     if (!hasSeenTour) {
       setIsVisible(true);
+      setTourStep(0); // Reset to first step on new visit
     }
   }, [hasSeenTour]);
 
